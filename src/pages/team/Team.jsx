28eada0635d2233111c42b6bda0619f0cc85035c
Wout De Maeseneer <wout.de.maeseneer@gmail.com>
2023-07-07 23:46:12 +0200

    feat: player names

--- src/pages/team/Team.jsx
+++ src/pages/team/Team.jsx
@@ -8,6 +8,32 @@ export default function Team() {
       <div id='Team_groepsfoto_crop'>
         <img src={groepsfoto} alt="zlatanks_groepsfoto" id='Team_groepsfoto' />
       </div>
+      <div id='player_overview_container'>
+        <div className='player_type' id='coach'>
+          <h2>Coach</h2>
+          <p>Rob De Putter</p>
+        </div>
+        <div className='player_type' id='keeper'>
+          <h2>Keeper</h2>
+          <p>Wout De Maeseneer</p>
+        </div>
+        <div className='player_type' id='defenders'>
+          <h2>Verdedigers</h2>
+          <p>Brikke Van de Velde</p>
+          <p>Jan Perdaens</p>
+          <p>Mauritz Lowie</p>
+          <p>Renaat De Cooman</p>
+          <p>Lander De Backer</p>
+        </div>
+        <div className='player_type' id='attackers'>
+          <h2>Aanvallers</h2>
+          <p>Wout Wartel</p>
+          <p>Mathias Luypaert</p>
+          <p>Senne De Backer</p>
+          <p>Jacques Baberyan</p>
+          <p>Thibault Lowie</p>
+        </div>
+      </div>
     </div>
   );
 }
