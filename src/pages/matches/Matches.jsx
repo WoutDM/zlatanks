9b6a95b8d4d6ecfdc9b6af689013ea88e929b4dc
Wout De Maeseneer <wout.de.maeseneer@gmail.com>
2024-06-09 18:00:11 +0200

    feat: replace non-updated games with a message

--- src/pages/matches/Matches.jsx
+++ src/pages/matches/Matches.jsx
@@ -4,11 +4,26 @@ import MatchComponent from './components/MatchComponent';
 
 export default function Matches() {
 
+  var noResultsAvailable = false;
+  var count = 0;
+
   return (
     <div id='Matches_body'>
       {
         matches.season20232024.map((match) => {
-          return (<MatchComponent match={match} />)
+          if (match.date === '6-1-2024') noResultsAvailable = true;
+          if (!noResultsAvailable) {
+            return (<MatchComponent match={match} />)
+          } 
+          if (noResultsAvailable && count === 0) {
+            count++;
+            return (
+                    <h3 className='text-center'>
+                      Didn't bother to update the other games, either lost or won against some shitty team.<br/>
+                      Check our Instagram for more results.
+                    </h3>
+                  )
+          }
         })
       }
     </div>
